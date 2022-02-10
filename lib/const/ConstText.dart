8c6e99d023d94c1ec2ea3f0a642d2b15325e1fce
Anmol Mishra <tbsdev3@gmail.com>
2022-02-10 17:48:02 +0530

    New Task Page

--- lib/const/ConstText.dart
+++ lib/const/ConstText.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class ConstText {
   static final String Consttitle = 'Task Manager';
+  static final String NewTasktitleAppBar = 'Create task';
+  static final String NewTasktitle = 'Task title';
+  static final String NewTaskCategory = 'Category';
+
 }
--- lib/const/colour.dart
+++ lib/const/colour.dart
@@ -6,4 +6,6 @@ class ConstColor {
   static const Color CircleAvatarImageBackGround = Color(0xFF76a6b8);
   static const ScaffoldbackgroundColor = Color.fromARGB(255, 243, 242, 248);
   static const Color ListBoxColor = Color(0xFF76a6b8);
+  static const Color NewTaskAppBarColor = Colors.white;
+  static const Color NewTaskTextAppBarColor = Colors.black;
 }
