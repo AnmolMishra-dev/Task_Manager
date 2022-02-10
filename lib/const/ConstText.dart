6913b32b6ccf92fc11ba29a49d38c33e08392083
Anmol Mishra <tbsdev3@gmail.com>
2022-02-10 17:04:11 +0530

    Task-Manager

---
+++ lib/const/ConstText.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ConstText {
+  static final String Consttitle = 'Task Manager';
+}
---
+++ lib/const/colour.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ConstColor {
+  static const Color AppBarBackground = Color(0xFF76a6b8);
+
+  static const Color CircleAvatarImageBackGround = Color(0xFF76a6b8);
+  static const ScaffoldbackgroundColor = Color.fromARGB(255, 243, 242, 248);
+  static const Color ListBoxColor = Color(0xFF76a6b8);
+}
